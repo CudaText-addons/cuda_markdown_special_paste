c15f46ad750ad8c6295edad5016f3fd0cc40df3b
Alexey <support@uvviewsoft.com>
2022-08-21 10:11:26 +0300

    +

--- __init__.py
+++ __init__.py
@@ -1,24 +1,60 @@
 import os
 from cudatext import *
 import cudatext_cmd as cmds
+import requests
 
 FORMATS = {
-    'Markdown': '[Text]({url})',
-    'reStructuredText': '`Text <{url}>`__',
+    'Markdown': '[{title}]({url})',
+    'reStructuredText': '`{title} <{url}>`__',
 }
 
+def dbg(s):
+    #print(s)
+    pass
+
+def get_title(s):
+    n = s.find('<title>')
+    if n<0:
+        dbg('no <title>: '+s)
+        return
+    s = s[n+7:]
+    n = s.find('</title>')
+    if n<0:
+        dbg('no </title>: '+s)
+        return
+    s = s[:n]
+    return s
+
+
 class Command:
-    
+
     def on_paste(self, ed_self, keep_caret, select_then):
         s = app_proc(PROC_GET_CLIP, '')
         if '\n' in s:
             return
         if not s.startswith('http://') and not s.startswith('https://'):
             return
+
+        try:
+            r = requests.get(s)
+        except:
+            return
+
+        if not r:
+            return
+        text = r.content.decode('utf-8')
+        if not text:
+            return
+
+        title = get_title(text)
+        if not title:
+            return
+
         lex = ed.get_prop(PROP_LEXER_FILE)
         fmt = FORMATS.get(lex)
         if not fmt:
             return
         fmt = fmt.replace('{url}', s)
+        fmt = fmt.replace('{title}', title)
         ed.cmd(cmds.cCommand_TextInsert, fmt)
         return False
